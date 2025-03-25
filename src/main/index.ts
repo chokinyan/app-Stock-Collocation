acf7a7b47b79206729d1126bf4e946ffa57bc520
SaberL <55279869+chokinyan@users.noreply.github.com>
2025-03-25 11:53:50 +0100

    Initialize project structure with TypeScript, Electron, and basic styling

---
+++ src/main/index.ts
@@ -0,0 +1,40 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    fullscreen: true,
+    resizable: false,
+    autoHideMenuBar: true,
+    /*icon: path.join(__dirname, '../../public/asset/image/icon/favico.ico'),*/
+    title: 'Projet Stock Collocation',
+    webPreferences: {
+      preload: path.join(__dirname, '../preload/preload.js'), // Chemin vers le preload
+      contextIsolation: true, // Sécurité : isolement du contexte
+      nodeIntegration: false, // Sécurité : désactive l'intégration de Node.js dans le renderer
+      devTools: false, // Ouvre les outils de développement
+    },
+  });
+
+  // Charge le fichier HTML
+  mainWindow.loadFile(path.join(__dirname, '../../public/index.html'));
+}
+
+app.on('ready', () => {
+  createWindow();
+
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
