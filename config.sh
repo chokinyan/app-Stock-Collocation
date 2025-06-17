b38629c96de7cb5656f5163516441f078506497d
chokinyan <sabou2344@gmail.com>
2025-06-17 13:01:59 +0200

    mise a jour du css

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
 npm install
 python3 -m venv --system-site-packages python-env
+active python-env/bin/activate
 pip install flask face_recognition opencv-python mysql-connector-python mfrc522 picamera2 RPi.GPIO imutils
